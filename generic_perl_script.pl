4aae48eb698f96e917f91182bd1e58ce8375b7c2
The Doctor <drwho@virtadpt.net>
2014-09-10 20:22:02 -0700

    De-personalized the generic Perl script.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- generic_perl_script.pl
+++ generic_perl_script.pl
@@ -2,10 +2,7 @@
 
 # name of script and what it does.
 
-# by: The Doctor [412/724/301/703]
-# 	<drwho [at] mx-missile [dot] virtadpt [dot] net>
-#	PGP key ID: 807B17C1
-#	PGP key fingerprint: 7960 1CDC 85C9 0B63 8D9F  DD89 3BD8 FF 2B 807B 17C1
+# by: 
 #
 # Just like Iraq, there are no weapons of mass destruction in my e-mail address.
 
