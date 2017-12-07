10e19774c478f3992fe2bb9763898e66ec9442db
The Doctor <drwho@virtadpt.net>
2017-12-07 15:18:12 -0800

    Initial commit of a generic Go file.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

---
+++ generic_go.go
@@ -0,0 +1,30 @@
+// Name of utility or module.
+// Description
+
+// by: <name> [e-mail address]
+// PGP key ID
+
+// Name of this package.
+package packagename
+
+// Import packages.
+import (
+	"fmt"
+	"os"
+)
+
+// Constants go here.
+const ()
+
+// Structs go here.
+type foo struct {
+}
+
+// Functions go here.
+func function() return_value {
+}
+
+// Start the paty.
+func main() {
+	os.Exit(0)
+}
